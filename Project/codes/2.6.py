cc5566c0f1acfcf236f1eb22b2902f3513eadb83
maniebra <mani.ebra@gmail.com>
2025-01-25 12:33:10 +0330

    finalized the project

---
+++ Project/codes/2.6.py
@@ -0,0 +1,23 @@
+def register(user_id, name, email, password, phone):
+    pass
+
+def login(user_id, password):
+    pass
+
+def create_class(class_id, name, description, instructor_id):
+    pass
+
+def add_student_to_class(class_id, student_id):
+    pass
+
+def set_grading_system(class_id: int, grading_system: dict):
+    pass
+
+def submit_grade(class_id, student_id, section):
+    pass
+
+def save_program_data():
+    pass
+
+def load_program_data():
+    pass
